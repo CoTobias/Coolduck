5f5e7120ca5116f7a26d86d002c675a488d23c7b
Tobias <coptiltobias@gmail.com>
2024-03-11 21:41:40 +0100

    Comments

--- packages/my_package/src/Trajectory.py
+++ packages/my_package/src/Trajectory.py
@@ -10,121 +10,129 @@ import matplotlib.pyplot as plt
 
 
 class Trajectory(DTROS):
+    # Duckiebot starts at Origin with direction "East"
     coordinates = [(0.0, 0.0)]
+    # Graph that will look like the track the bot drives on -> Also starts at Origin with same direction
     track_coordinates = [(0.0, 0.0)]
-    direction = ''
-    moverX = []
-    moverY = []
-    Old_left_ticks = 0
-    Old_right_ticks = 0
-    slope = []
+    # Sequence of tracks traversed
+    track_segments = []
+    # used for Change caluclation
+    prev_left_ticks = 0
+    prev_right_ticks = 0
+    # Indicates if we have finished a track: (2 seconds passed without tick change in any wheel)
     end_of_track = False
+    # used for end_of_track calculation
     right_Ticks_change_Number = 0
     left_Ticks_change_Number = 0
+    # gets incremented for track detection
     start_time = 0
     end_time = 0
-    track_segments = []
-    zähler = 0
+    # Important for calculation as slope calc. differs from east-west movement to North-South movement
     east_west = True
-    prev_x = 0
-    prev_y = 0
 
     def __init__(self, node_name, wheel_radius, wheel_distance, slippage_factor, speed):
-        # initialize the DTROS parent class
+        # Initialize the DTROS parent class
         super(Trajectory, self).__init__(node_name=node_name, node_type=NodeType.PERCEPTION)
-        # static parameters
+        # Vehicle name
         self._vehicle_name = os.environ['VEHICLE_NAME']
+        # Left and Right Encoder respectively
         self._left_encoder_topic = f"/{self._vehicle_name}/left_wheel_encoder_node/tick"
         self._right_encoder_topic = f"/{self._vehicle_name}/right_wheel_encoder_node/tick"
-
-        self._publisher = rospy.Publisher('chatter', String, queue_size=10)
-
-        # temporary data storage
-        self._ticks_left = None
-        self._ticks_right = None
-        # construct subscriber
+        # Subscriber for the Encoders
         self.sub_left = rospy.Subscriber(self._left_encoder_topic, WheelEncoderStamped, self.callback_left)
         self.sub_right = rospy.Subscriber(self._right_encoder_topic, WheelEncoderStamped, self.callback_right)
-
-        # Visualisation stuff
-        self.speed = speed
+        # Publisher used for publishing the Information for Mobile Application
+        self._publisher = rospy.Publisher('chatter', String, queue_size=10)
+        # Value of Encoders at current point in time
+        self._ticks_left = None
+        self._ticks_right = None
+        # Wheel Radius of the Duckiebot: Changes will impact the whole graph
         self.wheel_radius = wheel_radius
+        # Distance between the wheels, not initialized here but in the constructor of our node
         self.wheel_distance = wheel_distance
-        self.slippage_factor = slippage_factor
-
-        self.right_ticks_change = 0
-        self.left_ticks_change = 0
-
-        self.left_ticks_change_2 = 0
-        self.right_ticks_change_2 = 0
-
-        self.delta_x = 0  # Initialize delta_x
-        self.delta_theta = 0  # Initialize delta_theta
-        self.a = False
-
+        # Coordinate x and y at current moment in time
         self.x = 0
         self.y = 0
+        # Previous x and y coordinates
+        self.prev_x = 0
+        self.prev_y = 0
+        # Theta is used for orientation (direction) of the bot
         self.theta = 0
         self.prev_theta = 0
-        # distance per tick traveled
+        # Distance per tick traveled -> 135 ticks per Rotation
         self.distance_per_count = (2 * math.pi * self.wheel_radius) / 135
-
+        # The Duckiebot does not return to zero with the value of each encoder ocne it has started. To avoid having
+        # to start with a coordinate at 1000... or having to calculate extensivly I just subtract the first value
+        # from the encoder value and get 0 as starting position then
         self.firstRight_tick = None
         self.firstLeft_tick = None
 
+    # partly taken from official Duckietown Documentation:
+    # https://docs.duckietown.com/daffy/devmanual-software/beginner/ros/wheel-encoder-reader.html
     def callback_left(self, data):
-        # log general information once at the beginning
+        # Log general information once at the beginning to make sure everything works properly
         rospy.loginfo_once(f"Left encoder resolution: {data.resolution}")
         rospy.loginfo_once(f"Left encoder type: {data.type}")
-        # store data value
+        # General Case -> used to get Encoder Start value to 0
         if self._ticks_left is not None:
             self._ticks_left = data.data - self.firstLeft_tick
+        # First time calculation of Encoder
         else:
             self._ticks_left = data.data
             self.firstLeft_tick = self._ticks_left
 
+    # partly taken from official Duckietown Documentation:
+    # https://docs.duckietown.com/daffy/devmanual-software/beginner/ros/wheel-encoder-reader.html
     def callback_right(self, data):
-        # log general information once at the beginning
+        # Log general information once at the beginning to make sure everything works properly
         rospy.loginfo_once(f"Right encoder resolution: {data.resolution}")
         rospy.loginfo_once(f"Right encoder type: {data.type}")
-        # store data value
+        # General Case -> used to get Encoder Start value to 0
         if self._ticks_right is not None:
             self._ticks_right = data.data - self.firstRight_tick
+        # First time calculation of Encoder
         else:
             self._ticks_right = data.data
             self.firstRight_tick = self._ticks_right
 
+    # Method that called in the run method -> Calls different methods to find out coordinates at current pos.
     def update(self):
-        # call the update encoder and calculation method indirectly
+        # Call the update encoder and calculation method indirectly
         result = self.update_encoder_ticks()
+        # Just when change happens coordinates get appended, or else we would have 1000 points at the same value
         if result is not None:
             x, y = result
             Trajectory.coordinates.append((x, y))
 
+    # Method that occupies with change of encoder value
     def update_encoder_ticks(self):
-        left_ticks_change = self._ticks_left - Trajectory.Old_left_ticks
-        right_ticks_change = self._ticks_right - Trajectory.Old_right_ticks
-
+        left_ticks_change = self._ticks_left - Trajectory.prev_left_ticks
+        right_ticks_change = self._ticks_right - Trajectory.prev_right_ticks
+        # Logic: When nothing changes for > 2 seconds for either encoder -> Track is finished
         if left_ticks_change == 0 and right_ticks_change == 0:
             Trajectory.right_Ticks_change_Number = Trajectory.right_Ticks_change_Number + 1
             Trajectory.left_Ticks_change_Number = Trajectory.left_Ticks_change_Number + 1
-            # detect if end of track if no movement for more than 2 seconds (20HZ -> 20 messages / second)
+            # Detect if end of track if no movement for more than 2 seconds (20HZ -> 20 messages / second)
             if Trajectory.right_Ticks_change_Number == 40 and Trajectory.left_Ticks_change_Number == 40:
                 Trajectory.end_of_track = True
             return None
+        # Logic: If stuff changes it is on the track and moves
         else:
-            # moving again so no end of track
+            # Moving again so no end of track
             Trajectory.left_Ticks_change_Number = 0
             Trajectory.right_Ticks_change_Number = 0
-            # every time we calculate something increase the end time
+            # Every time we calculate something increase the end time
             Trajectory.end_time += 1
-            # set old Trajectory to the updates value for next run
-            Trajectory.Old_left_ticks = self._ticks_left
-            Trajectory.Old_right_ticks = self._ticks_right
-            # use another method solely for calculation
+            # Set old Trajectory to the updates value for next run
+            Trajectory.prev_left_ticks = self._ticks_left
+            Trajectory.prev_right_ticks = self._ticks_right
+            # Use another method solely for calculation
             return self._calculate_coordinates(left_ticks_change, right_ticks_change)
             # Track begins again or make sure that not end of track detected
 
+    # Actual Calculation of coordinates of robot with differential steering:
+    # Elementary Trajectory Model for the Differential Steering System of Robot Wheel Actuators
+    # Source: https://rossum.sourceforge.net/papers/DiffSteer/
     def _calculate_coordinates(self, left_ticks_change, right_ticks_change):
         # distance traveled with average distance per count calculated through distance_traveled/tick
         sl = self.distance_per_count * left_ticks_change
@@ -168,29 +176,29 @@ class Trajectory(DTROS):
                         line = self.generate_straight_line(start_x, start_y, start_x, end_y, 50)
                         Trajectory.track_coordinates.extend(line)
                     # assign values
-                    Trajectory.prev_y = start_y
-                    Trajectory.prev_x = end_x
+                    self.prev_y = start_y
+                    self.prev_x = end_x
                 elif pattern == "LEFT CURVE":
                     # adjust distance to adjust curvature
                     distance = 25
                     # Calculate midpoint of the line segment
-                    mid_x = (Trajectory.prev_x + end_x) / 2
-                    mid_y = (Trajectory.prev_y + end_y) / 2
+                    mid_x = (self.prev_x + end_x) / 2
+                    mid_y = (self.prev_y + end_y) / 2
 
                     # Calculate the angle of the line segment
-                    angle = math.atan2(end_y - Trajectory.prev_y, end_x - Trajectory.prev_x)
+                    angle = math.atan2(end_y - self.prev_y, end_x - self.prev_x)
 
                     # Calculate control point
                     control_x = mid_x + distance * math.cos(angle - math.pi / 2)
                     control_y = mid_y + distance * math.sin(angle - math.pi / 2)
 
-                    curve_points = self.generate_bezier_curve(Trajectory.prev_x, Trajectory.prev_y, end_x, end_y, control_x,
+                    curve_points = self.generate_bezier_curve(self.prev_x, self.prev_y, end_x, end_y, control_x,
                                                               control_y, 80)
                     Trajectory.track_coordinates.extend(curve_points)
 
                     # assign last value tp prev value
-                    Trajectory.prev_x = end_x
-                    Trajectory.prev_y = end_y
+                    self.prev_x = end_x
+                    self.prev_y = end_y
         print(Trajectory.track_coordinates)
         print("----------------------------------------------------------------------------------")
 
@@ -265,9 +273,9 @@ class Trajectory(DTROS):
             if self._ticks_right is not None and self._ticks_left is not None:
                 self.update()
                 self.analyze_track()
+                message = f"{Trajectory.coordinates}!"
+                self._publisher.publish(message)
                 if Trajectory.end_of_track:
-                    message = f"{Trajectory.coordinates}!"
-                    self._publisher.publish(message)
                     print(f"Track segments traversed: {Trajectory.track_segments}")
                     print(Trajectory.coordinates)
                     print("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
